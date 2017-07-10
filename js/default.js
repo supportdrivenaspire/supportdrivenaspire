c729449e906ac94a3e525f20fbd0a513e28bbc7f
Scott Tran <scott.tran@gmail.com>
2017-07-10 07:17:57 -0700

    add nykki and snack testimonials

--- js/default.js
+++ js/default.js
@@ -7,21 +7,6 @@ WebFont.load({
   }
 });
 
-// Logo Animation
-$svg = $('.logo > svg').drawsvg({
-  duration: 1500,
-  callback: function() {
-    $('.logo').addClass('active');
-  }
-});
-
-function animateLogo() {
-  $svg.drawsvg('animate');  
-}
-
-animateLogo();
-
-
 // Swipe Controls for Carousel
 $('.carousel').swiperight(function() {
   $(this).carousel('prev');
@@ -29,25 +14,3 @@ $('.carousel').swiperight(function() {
 $('.carousel').swipeleft(function() {
   $(this).carousel('next');
 });
-
-// Google Map
-function googleMap() {
-  var map = document.getElementById('google-map');
-
-  var map_options = {
-    center: new google.maps.LatLng(-33.867363,151.183539),
-    scrollwheel: false,
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  }
-  var map = new google.maps.Map(map, map_options)
-
-  // Map Marker
-  var myLatlng = new google.maps.LatLng(-33.867363,151.183539);
-  var marker = new google.maps.Marker({
-    position: myLatlng,
-    map: map,
-    icon: 'img/location-pin.svg'
-  });
-}
-google.maps.event.addDomListener(window, 'load', googleMap);
